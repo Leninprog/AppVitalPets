630ad10d43a4732caf1d776841d40cf91a2b49e6
Leninprog <lenin.arevalo@udla.edu.ec>
2025-06-05 21:49:49 -0500

    Agregar archivos de proyecto.

---
+++ APPVitalPets/APPVitalPets/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APPVitalPets
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ APPVitalPets/APPVitalPets/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace APPVitalPets
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
